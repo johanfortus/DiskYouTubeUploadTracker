b2a2fc7d5c8957421c466319cb9d07042379e829
johanfortus <fortusjohan@gmail.com>
2025-03-14 00:55:26 -0400

    Environment variables

--- bot.py
+++ bot.py
@@ -1,4 +1,21 @@
 import discord
+import requests
+import json
+import os
+import datetime
+from discord.ext import commands, tasks
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
+
+CHANNELS = {
+    "Vro" : os.getenv("CHANNEL_ID_ONE"),
+    "Knight" : os.getenv("CHANNEL_ID_TWO"),
+    "Splash" : os.getenv("CHANNEL_ID_THREE")
+}
 
 class Client(discord.Client):
     async def on_ready(self):
@@ -8,4 +25,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run('')
+client.run(DISCORD_TOKEN)
