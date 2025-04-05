eab26a469c14808b483eb516392e270b513f9780
johanfortus <fortusjohan@gmail.com>
2025-04-05 16:42:56 -0400

    Determine video type

--- uploadchecker.py
+++ uploadchecker.py
@@ -14,26 +14,29 @@ class UploadChecker:
         self.LAST_UPLOADS = []
 
 
-    def is_short(self, video_id):
+    def video_type(self, video_id):
         url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&part=contentDetails&key={self.YOUTUBE_API_KEY}"
         res = requests.get(url).json()
 
         duration = res["items"][0]["contentDetails"]["duration"]
         parsed_duration = str(isodate.parse_duration(duration)).split(":")
 
-        hours = parsed_duration[0]
-        minutes = parsed_duration[1]
-        seconds = parsed_duration[2]
+        hours, min, sec = parsed_duration[0], parsed_duration[1], parsed_duration[2]
 
-        print(f"Hours: {hours}")
-        print(f"Minutes: {minutes}")
-        print(f"Seconds: {seconds}")
+        if int(hours) > 0:
+            return "livestream"
+        elif int(min) > 0:
+            return "longform"
+        elif int(sec) > 0:
+            return "short"
 
-        print(f"Parsed Duration: {parsed_duration}")
-
-
-
-        return "M" not in res["items"][0]["contentDetails"]["duration"]
+        # print(f"Hours: {hours}")
+        # print(f"Minutes: {min}")
+        # print(f"Seconds: {sec}")
+        #
+        # print(f"Parsed Duration: {parsed_duration}")
+        #
+        # return "M" not in res["items"][0]["contentDetails"]["duration"]
 
     def get_latest_videos(self, channel_id):
         pass
