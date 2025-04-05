e33a4742e255d5be5430275afd5f5f03b6cf195e
johanfortus <fortusjohan@gmail.com>
2025-04-05 16:57:06 -0400

    Video upload date match current date

--- uploadchecker.py
+++ uploadchecker.py
@@ -1,5 +1,6 @@
 import requests
 import isodate
+from datetime import date
 from constants import DISCORD_TOKEN, YOUTUBE_API_KEY, DISCORD_CHANNEL_ID, CHANNELS
 
 
@@ -30,18 +31,36 @@ class UploadChecker:
         elif int(sec) > 0:
             return "short"
 
-        # print(f"Hours: {hours}")
-        # print(f"Minutes: {min}")
-        # print(f"Seconds: {sec}")
-        #
-        # print(f"Parsed Duration: {parsed_duration}")
-        #
-        # return "M" not in res["items"][0]["contentDetails"]["duration"]
-
     def get_latest_videos(self, channel_id):
-        pass
+        longform_upload_status = "❌"
+        short_upload_status = "❌"
 
-    async def check_uploads(self):
+        current_date = str(date.today())
+        print(f"Today: {current_date}")
+
+        channel_url = f"https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id={channel_id}&key={YOUTUBE_API_KEY}"
+        res = requests.get(channel_url).json()
+
+        uploads_playlist_id = res["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+        playlist_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={uploads_playlist_id}&key={YOUTUBE_API_KEY}"
+        res = requests.get(playlist_url).json()
+
+        videos = res["items"]
+
+        for video in videos:
+
+            video_id = video["snippet"]["resourceId"]["videoId"]
+            video_title = video["snippet"]["title"]
+            video_upload_date = str(video["snippet"]["publishedAt"][0:10])
+
+            # print(f"Video Title: {video_title}")
+
+            if current_date == video_upload_date:
+                print(f"Title: {video_title} \nID: {video_id} \nUpload Date: {video_upload_date} \n")
+
+
+
+    def check_uploads(self):
         pass
 
 
