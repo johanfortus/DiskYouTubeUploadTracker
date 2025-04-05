da8411ea3f3c8130a277ae2b109203340d19bcd8
johanfortus <fortusjohan@gmail.com>
2025-04-05 16:12:24 -0400

    Check is short in uploadchecker

---
+++ constants.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+DISCORD_CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
+
+CHANNELS = {
+    "Vro": os.getenv("CHANNEL_ID_ONE"),
+    "Knight": os.getenv("CHANNEL_ID_TWO"),
+    "Splash": os.getenv("CHANNEL_ID_THREE")
+}
--- uploadchecker.py
+++ uploadchecker.py
@@ -1,18 +1,23 @@
+import requests
+from constants import DISCORD_TOKEN, YOUTUBE_API_KEY, DISCORD_CHANNEL_ID, CHANNELS
 
 
 class UploadChecker:
 
-    def __init__(self, YOUTUBE_API_KEY, DISCORD_CHANNEL_ID, CHANNELS):
+    def __init__(self, YOUTUBE_API_KEY, CHANNELS):
 
         self.YOUTUBE_API_KEY = YOUTUBE_API_KEY
-        self.DISCORD_CHANNEL_ID = DISCORD_CHANNEL_ID
 
         self.CHANNELS = CHANNELS
 
         self.LAST_UPLOADS = []
 
+
     def is_short(self, video_id):
-        pass
+        url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&part=contentDetails&key={self.YOUTUBE_API_KEY}"
+        res = requests.get(url).json()
+        print(f"Video ID: {video_id}")
+        return "M" not in res["items"][0]["contentDetails"]["duration"]
 
     def get_latest_videos(self, channel_id):
         pass
@@ -20,3 +25,6 @@ class UploadChecker:
     async def check_uploads(self):
         pass
 
+
+
+UploadChecker = UploadChecker(YOUTUBE_API_KEY, CHANNELS)
