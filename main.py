8cf8f8700f2cdc9532907dd1b88ed0763e900d80
johanfortus <fortusjohan@gmail.com>
2025-04-06 01:19:12 -0400

    Initialize main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-
+import discord
+import requests
+import os
+from datetime import date
+from dotenv import load_dotenv
+from discord.ext import tasks
 
 def main():
 
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
 
 if __name__ == "__main__":
     main()
